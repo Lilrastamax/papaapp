cfee27a83717429941713334bdeabaa687eaada5
Max <baert.maxime@hotmail.fr>
2026-08-11 22:10:11 +0200

    Fix chemins relatifs pour GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'papaapp-v9';
-const STATIC = ['/','/index.html','/app.css','/app.js','/manifest.json','/icon-192.png','/icon-512.png'];
+const STATIC = ['/','index.html','app.css','app.js','manifest.json','icon-192.png','icon-512.png'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
