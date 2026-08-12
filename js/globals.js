6dae2a5faf9dcdd17ce4c63c69f5d6f6d161d2f5
Max <baert.maxime@hotmail.fr>
2026-08-12 19:15:25 +0200

    refactor: ajoute le pont window.* pour les onclick inline

---
+++ js/globals.js
@@ -0,0 +1,32 @@
+import { DB, S, saveDB, delFrom } from './store.js';
+import { cloudPushSettings } from './api.js';
+import { render } from './render.js';
+import { navigate } from './nav.js';
+import { showSettings, showSchoolEditModal, showExtraVisitModal, showSundayOverrideModal, closeM } from './modal.js';
+import { showCustodyModal } from './screens/agenda.js';
+import { addApptForDate } from './screens/home.js';
+import { viewDocument } from './render.js';
+import { doRecurringTask } from './screens/misc.js';
+import { toast } from './ui.js';
+import { exportData } from './main.js';
+
+export function exposeGlobals() {
+  window.DB = DB;
+  window.S = S;
+  window.saveDB = saveDB;
+  window.delFrom = delFrom;
+  window.cloudPushSettings = cloudPushSettings;
+  window.render = render;
+  window.navigate = navigate;
+  window.showSettings = showSettings;
+  window.showSchoolEditModal = showSchoolEditModal;
+  window.showExtraVisitModal = showExtraVisitModal;
+  window.showCustodyModal = showCustodyModal;
+  window.showSundayOverrideModal = showSundayOverrideModal;
+  window.addApptForDate = addApptForDate;
+  window.viewDocument = viewDocument;
+  window.doRecurringTask = doRecurringTask;
+  window.exportData = exportData;
+  window.closeM = closeM;
+  window.toast = toast;
+}
